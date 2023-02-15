9838e45b6d78d144d225d7d629fcedc62b96b8be
blake <hill2b7@mail.uc.edu>
2023-02-15 14:10:16 -0500

    Lab 06 Task 04

---
+++ src/metric.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class metric
+{
+    public static void main(String[] args)
+    {
+        double meters = 0;
+        double inches = 0;
+        double feet = 0;
+        double miles = 0;
+        String trash = "";
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a value in meters: ");
+        if (in.hasNextDouble())
+        {
+            meters = in.nextDouble();
+            feet = meters * 3.28;
+            miles = meters / 1609.34;
+            inches = meters * 39.37;
+            System.out.printf(meters + " meters converts to %1.2f in feet \n", feet );
+            System.out.printf(meters + " meters converts to %1.8f in miles \n", miles);
+            System.out.printf(meters + " meters converts to %1.2f in miles \n", inches);
+        }
+        else
+        {
+            System.out.println("You have entered an incorrect value ");
+            trash = in.nextLine();
+        }
+
+    }
+}
