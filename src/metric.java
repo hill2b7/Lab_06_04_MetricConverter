0c0255d93db1ead371cfaa14dcc3243182e09797
blake <hill2b7@mail.uc.edu>
2023-02-15 15:22:59 -0500

    Lab 06 Task 04

--- src/metric.java
+++ src/metric.java
@@ -4,29 +4,30 @@ public class metric
 {
     public static void main(String[] args)
     {
-        double meters = 0;
-        double inches = 0;
-        double feet = 0;
-        double miles = 0;
-        String trash = "";
+        double meters = 0; //declare variable
+        double inches = 0; //declare variable
+        double feet = 0; //declare variable
+        double miles = 0; //declare variable
+        String trash = ""; //declare variable
 
-        Scanner in = new Scanner(System.in);//imports scanne
+        Scanner in = new Scanner(System.in);//imports scanner
 
-        System.out.println("Enter a value in meters: ");
-        if (in.hasNextDouble())
+        System.out.println("Enter a value in meters: "); // asks user for input
+        if (in.hasNextDouble()) // if statement that checks to see if input is correct
         {
-            meters = in.nextDouble();
-            feet = meters * 3.28;
-            miles = meters / 1609.34;
-            inches = meters * 39.37;
-            System.out.printf(meters + " meters converts to %1.2f in feet \n", feet );
-            System.out.printf(meters + " meters converts to %1.8f in miles \n", miles);
-            System.out.printf(meters + " meters converts to %1.2f in miles \n", inches);
+            meters = in.nextDouble(); //sets meters to the input received from the user
+            feet = meters * 3.28; //formula to convert meters to feet
+            miles = meters / 1609.34; //formula to convert meters to miles
+            inches = meters * 39.37; //formula to convert meters to inches
+            System.out.printf(meters + " meters converts to %1.2f in feet \n", feet ); //outputs what the conversion is used printf to keep the decimal to two points
+            System.out.printf(meters + " meters converts to %1.8f in miles \n", miles); //outputs what the conversion is used printf to keep the decimal to eight points
+            System.out.printf(meters + " meters converts to %1.2f in miles \n", inches); //outputs what the conversion is used printf to keep the decimal to two points
         }
-        else
+        else // if input is not correct the code block below will run
         {
-            System.out.println("You have entered an incorrect value ");
-            trash = in.nextLine();
+            trash = in.nextLine(); // if a string is entered as an input it will store that value to trash
+            System.out.println("You have entered an incorrect value " + trash); // output that lets the user know that the input received was not expected
+
         }
 
     }
